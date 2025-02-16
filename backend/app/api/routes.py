42afe4997b9f75a148ab3fc43dedc1819721f407
V-Silpin <varun.mohanta323@gmail.com>
2025-02-16 18:02:41 +0530

    Changed the method of extraction

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -3,12 +3,15 @@ from fastapi.responses import JSONResponse
 from io import BytesIO
 import fitz
 from services.pdfer import extract_text_and_images
+from services.dbops import dbOps
 
 router = APIRouter()
+ops = dbOps()
 
 @router.post("/upload-pdf/")
 async def upload_pdf(file: UploadFile = File(...)):
     pdf_stream = BytesIO(await file.read())
     cont = extract_text_and_images(pdf_stream)
-
+    ops.insert_text(cont["text"])
+    ops.insert_img(cont["text"])
     return JSONResponse(content=cont)
